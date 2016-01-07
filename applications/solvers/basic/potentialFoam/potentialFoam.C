ba3e4eac79824b9d8cb5bc67e18ccb15312ea5c4
Henry Weller <http://cfd.direct>
2016-01-07 15:38:13 +0000

    potentialFoam: Remove unused fvOptions Resolves bug-report http://www.openfoam.org/mantisbt/view.php?id=1964

--- applications/solvers/basic/potentialFoam/potentialFoam.C
+++ applications/solvers/basic/potentialFoam/potentialFoam.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011-2015 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2016 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -36,7 +36,6 @@ Description
 
 #include "fvCFD.H"
 #include "pisoControl.H"
-#include "fvIOoptionList.H"
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
@@ -81,7 +80,6 @@ int main(int argc, char *argv[])
 
     #include "createFields.H"
     #include "createMRF.H"
-    #include "createFvOptions.H"
 
     // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
