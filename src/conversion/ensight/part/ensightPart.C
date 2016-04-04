ea3490b533b444dd42292c6dec940b03b3313ddc
Henry Weller <http://cfd.direct>
2016-04-04 08:57:42 +0100

    src/conversion/ensight: Specify std::isnan Resolves bug-report http://www.openfoam.org/mantisbt/view.php?id=2041

--- src/conversion/ensight/part/ensightPart.C
+++ src/conversion/ensight/part/ensightPart.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011-2013 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2015 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -51,7 +51,7 @@ bool Foam::ensightPart::isFieldDefined(const List<scalar>& field) const
         {
             const label id = idList[i];
 
-            if (id >= field.size() || isnan(field[id]))
+            if (id >= field.size() || std::isnan(field[id]))
             {
                 return false;
             }
@@ -136,9 +136,8 @@ Foam::autoPtr<Foam::ensightPart> Foam::ensightPart::New(Istream& is)
 
     if (cstrIter == istreamConstructorTablePtr_->end())
     {
-        FatalIOErrorIn
+        FatalIOErrorInFunction
         (
-            "ensightPart::New(Istream&)",
             is
         )   << "unknown ensightPart type "
             << partType << nl << nl
